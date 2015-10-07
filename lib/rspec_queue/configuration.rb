c3f1d2d6d0285f911e6905a410e5cc566de5cc1b
nickbrowne <nick.browne@theconversation.edu.au>
2015-10-07 17:29:12 +1100

    Add before and after fork configuration blocks

--- lib/rspec_queue/configuration.rb
+++ lib/rspec_queue/configuration.rb
@@ -4,6 +4,25 @@ module RSpecQueue
   class Configuration
     include Singleton
 
+    attr_accessor :after_fork_block
+    attr_accessor :before_fork_block
+
+    def self.after_fork(&block)
+      self.instance.after_fork_block = block
+    end
+
+    def self.before_fork(&block)
+      self.instance.before_fork_block = block
+    end
+
+    def self.call_before_fork_hooks
+      self.instance.before_fork_block.call if self.instance.before_fork_block
+    end
+
+    def self.call_after_fork_hooks(index)
+      self.instance.after_fork_block.call(index) if self.instance.after_fork_block
+    end
+
     def worker_count
       @worker_count ||= [env_queue_workers || cpu_count - 1, 1].max
     end
